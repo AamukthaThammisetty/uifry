ae8caf0a4234301736eb7cf74dbd9f5868e1e309
AamukthaThammisetty <aamukthasetty2005@gmail.com>
2024-06-03 17:49:29 +0530

    Text added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,65 +4,58 @@ const Footer = () => {
 
   const footerNavs = [
     {
-      label: "Company",
+      label: "Links",
       items: [
         {
           href: 'javascript:void()',
-          name: 'Partners'
+          name: 'Home'
+        },
+        {
+          href: 'javascript:void()',
+          name: 'About Us'
+        },
+        {
+          href: 'javascript:void()',
+          name: 'Bookings'
         },
         {
           href: 'javascript:void()',
           name: 'Blog'
         },
-        {
-          href: 'javascript:void()',
-          name: 'Team'
-        },
-        {
-          href: 'javascript:void()',
-          name: 'Careers'
-        },
       ]
     },
     {
-      label: "Resources",
+      label: "Legal",
       items: [
         {
           href: 'javascript:void()',
-          name: 'Contact'
+          name: 'Terms of Use'
         },
         {
           href: 'javascript:void()',
-          name: 'Support'
+          name: 'Privacy Policy'
         },
         {
           href: 'javascript:void()',
-          name: 'Docs'
+          name: 'Cookie Policy'
         },
-        {
-          href: 'javascript:void()',
-          name: 'Pricing'
-        },
+        
       ]
     },
     {
-      label: "About",
+      label: "Product",
       items: [
         {
           href: 'javascript:void()',
-          name: 'Terms'
+          name: 'Take Tour'
         },
         {
           href: 'javascript:void()',
-          name: 'License'
+          name: 'Live Chat'
         },
         {
           href: 'javascript:void()',
-          name: 'Privacy'
-        },
-        {
-          href: 'javascript:void()',
-          name: 'About Us'
+          name: 'Review'
         },
       ]
     },
@@ -79,12 +72,12 @@ const Footer = () => {
 
   return (
     <footer className="text-gray-500 bg-white px-4 py-5 max-w-screen-xl mx-auto md:px-8">
-      <div className="gap-6 justify-between md:flex">
-        <div className="flex-1">
+      <div className=" justify-center gap-16 md:flex">
+        <div>
           <div className="max-w-xs">
-            <img src="./logo.png" className="w-32" alt="Logo" />
-            <div pt-10>
-              <div className="flex gap-2 items-center"> 
+            <img src="./logo.png" className="w-32 pb-5" alt="Logo" />
+            <div>
+              <div className="flex gap-2 pb-4 items-center"> 
                 <div className="text-[#FF5555]">
                   <CiMail />
                 </div>
@@ -99,17 +92,17 @@ const Footer = () => {
             </div>
           </div>
         </div>
-        <div className="flex-1 mt-10 space-y-6 items-center justify-between sm:flex md:space-y-0 md:mt-0">
+        <div className=" mt-10 space-y-6 gap-16 justify-between text-black sm:flex md:space-y-0 md:mt-0">
           {
             footerNavs.map((item, idx) => (
-              <ul className="space-y-4" key={idx}>
-                <h4 className="text-gray-800 font-medium">
+              <ul className="space-y-4 " key={idx}>
+                <h4 className="text-black text-2xl font-medium">
                   {item.label}
                 </h4>
                 {
                   item.items.map((el, idx) => (
                     <li key={idx}>
-                      <a href={el.href} className="hover:underline hover:text-indigo-600">
+                      <a href={el.href} className="hover:underline hover:text-[#FF5555]">
                         {el.name}
                       </a>
                     </li>
@@ -126,10 +119,10 @@ const Footer = () => {
                         type="email"
                         required
                         placeholder="Enter your email"
-                        className="w-15 pl-12 pr-3 py-2 text-gray-500 bg-white outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+                        className="w-15 pl-12 pr-3 py-2 text-black bg-white outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
                       />
                     </div>
-                    <button className=" w-auto py-3 px-4 font-medium text-sm text-center text-white bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-700 active:shadow-none rounded-lg shadow mt-2">
+                    <button className=" w-auto py-3 px-4 font-medium text-sm text-center text-white bg-black active:shadow-none rounded-lg shadow mt-2">
                       Subscribe
                     </button>
                     </div>
@@ -140,9 +133,9 @@ const Footer = () => {
           }
         </div>
       </div>
-      <div className="mt-8 py-6 border-t items-center justify-between sm:flex">
+      <div className="mt-8 py-6 border-t border-black items-center text-black font-bold justify-center sm:flex">
         <div className="mt-4 sm:mt-0">
-          &copy; 2022 uifry UI All rights reserved.
+        Copyright 2022 uifry.com all rights reserved
         </div>
       </div>
     </footer>
